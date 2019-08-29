5600daa48943e17034a53644f0f3eb7d8cc7b6a5
yannafaith <yannafaith@gmail.com>
2019-08-29 06:46:34 -0400

    recurring bill functionality working

--- dev/src/pages/Finances.jsx
+++ dev/src/pages/Finances.jsx
@@ -2,39 +2,51 @@ import React, { useState } from 'react';
 
 const Finances = () => {
 
-    const [ liveBalance ] = useState( 0 );
-    const [ projBalance ] = useState( '5,500' );
-    const [ bills, setBills ] = useState( [ 0 ] );
-    const [ newBill, setNewBill ] = useState( '' );
-    // const [ billName, setBillName ] = useState( '' );
+    let [ monthlyDebt, setMonthlyDebt ] = useState( 0 );
+    const [ projBalance ] = useState( 5400 );
+    const [ bills, setBills ] = useState( [] );
+    const [ projlen ] = useState( 2 );
 
-    const monthlyDebit = bills.reduce( ( bill, sum ) => sum += bill ); 
+    const [ newBill, setNewBill ] = useState( {
+        'name': 'test',
+        'value': 0
+    });
 
     const addBill = (e) => {
         e.preventDefault();
         setBills( [...bills, newBill ] );
-        setNewBill('');
+        setMonthlyDebt( monthlyDebt += newBill.value );
+        setNewBill( { ...newBill, name: 'test', value: 0 } );
     }
+
+    let remBalance = projBalance - monthlyDebt * projlen;
 
     return (
         <div>
-
             <h1> Finances </h1>
-            <h3> Your current balance is ${ liveBalance } </h3>
             <h3> Your balance used for projections is ${ projBalance } </h3>
-            <h2> Recurring Bills: { bills.map( ( bill, idx ) => <p key={ idx }> ${ bill } </p> )} </h2>
-            <h2> Total monthly debit = ${ monthlyDebit } </h2>
+            <h2> Recurring Bills </h2> 
+
+            {
+                bills.map( bill => {
+                    return <p> { bill.name } = ${bill.value } </p>
+                })
+            }
 
             <form onSubmit={ addBill }>
-                <div>
-                    {/* <input onChange = { ( e ) => setBillName( e.target.value )} /> */}
-                    <input 
-                        value = { newBill }
-                        onChange = { ( e ) => setNewBill( Number( e.target.value ) )} 
-                    />
-                </div>
+                <input 
+                    placeholder = 'bill name'
+                    onChange = { ( e ) => setNewBill( { ...newBill, name: e.target.value } ) } 
+                />
+                <input 
+                    value = { newBill.value }
+                    onChange = { ( e ) => setNewBill( { ...newBill, value: Number( e.target.value ) } ) } 
+                />
+                <button type='submit'> Add Bill </button>
             </form>
 
+            <h2> Total monthly debit = ${ monthlyDebt } </h2>
+            <h2> Remaining balance = ${ remBalance } </h2>
         </div>
     );
 }
